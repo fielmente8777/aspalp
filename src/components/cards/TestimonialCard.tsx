e9ba441edbf444533e1a2df70227e39dea18f50e
kajal021 <kajal@eazotel.com>
2026-08-17 11:01:28 +0530

    chatbot name changed

--- src/components/cards/TestimonialCard.tsx
+++ src/components/cards/TestimonialCard.tsx
@@ -7,7 +7,7 @@ const TestimonialCard: React.FC<TestimonialDataType["testimonials"][0]> = ({
   rating,
 }) => {
   return (
-    <div className="rounded-2xl  backdrop-blur-sm border border-gold bg-background-dark-2 p-6 flex flex-col gap-5">
+    <div className="rounded-2xl  backdrop-blur-sm border border-gold bg-white p-6 flex flex-col gap-5">
       <p className="text-[#c8a96a]">★★★★★</p>
       <p className="text-dark">{review}</p>
       <div className="w-full h-px bg-gold" />
