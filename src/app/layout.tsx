b125e733e089119035d890c101169faf2b586c35
kajal021 <kajal@eazotel.com>
2026-09-04 17:28:18 +0530

    Integrate Google Tag Manager for analytics tracking
    Added Google Tag Manager and noscript fallback for analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} ${manrope.variable} ${cormorant.variable} ${instrumentSerif.variable} h-full antialiased`}
     >
       <head>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W8RVGPSV');`}</Script>
         {/* <!-- Google tag (gtag.js) --> */}
         <Script
           id="analytics"
@@ -58,6 +66,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         </Script>
       </head>
       <body suppressHydrationWarning={true}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W8RVGPSV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <WebProvider>
           <LandingNavbar />
           {children}
